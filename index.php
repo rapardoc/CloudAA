95774e1eb4c4483d8f63a1236b517954b47cd14e
rapardoc <rapardoc@outlook.com>
2024-02-13 16:56:24 -0600

    mensaje 2

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
          <div class="container">
             <div class="knowledge_main">
                <div class="left_main">
-                  <h1 class="knowledge_taital">Knowledge of center</h1>
+                  <h1 class="knowledge_taital">Knowledge of center 2</h1>
                   <p class="knowledge_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
                </div>
                <div class="right_main">
